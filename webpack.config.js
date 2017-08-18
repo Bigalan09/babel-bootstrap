7c6b73a6d488ec536f02f061cbcc4cd7e78d4557
Bigalan09 <bigalan09@gmail.com>
2017-08-18 10:24:05 +0100

    ES6 babel bootstrap code

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    pathinfo: true,
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: './dist/',
+    filename: 'bundle.js'
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
